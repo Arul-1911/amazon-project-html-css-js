4c546410b983269abac261c8277a1d86e2536df2
Arul-1911 <arulpraveen19112002@gmail.com>
2024-02-14 18:29:03 +0530

    generated html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productid:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  quantity:2
+},{
+  productid:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:1
+}];
 
 //addtocart functionality function
 export function addToCart(productid){
