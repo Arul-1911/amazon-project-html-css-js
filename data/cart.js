2993e756bf0a054bd1bb9b89bbc54b53972d44e6
Arul-1911 <arulpraveen19112002@gmail.com>
2024-02-14 19:09:27 +0530

    price decimal fixed and radio selectors fixed

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,5 @@
 export const cart = [{
-  productid:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  productid:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity:2
 },{
   productid:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
