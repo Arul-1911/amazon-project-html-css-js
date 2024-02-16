1013bff6c22d4778846790023ae2ab3c76468a0e
Arul-1911 <arulpraveen19112002@gmail.com>
2024-02-16 18:58:27 +0530

    generated html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ if (!cart) {
     {
       productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionsId:'1'
     },
     {
       productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionsId:'2'
     },
   ];
 }
@@ -39,6 +41,7 @@ export function addToCart(productid) {
     cart.push({
       productid,
       quantity,
+      deliveryOptionsId:'1'
     });
   }
 
