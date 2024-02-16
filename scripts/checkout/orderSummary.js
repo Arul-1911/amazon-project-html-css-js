ed5cf2d2fe6b8865f6262db54b01c27db7360952
Arul-1911 <arulpraveen19112002@gmail.com>
2024-02-16 21:24:51 +0530

    generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,10 +5,10 @@ import {
     updateQuantity,
     updateDeliveryOption
   } from "../../data/cart.js";
-  import { products } from "../../data/products.js";
+  import { products, getProduct } from "../../data/products.js";
   import { formatCurrecncy } from "../utils/money.js";
   import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-  import { deliveryOptions } from "../../data/deliveryOptions.js";
+  import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
   
   export function renderOrderSummary(){
   
@@ -17,24 +17,14 @@ import {
   cart.forEach((cartItem) => {
     const productId = cartItem.productid;
   
-    let matchingProduct;
+    let matchingProduct = getProduct(productId);
   
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+   
   
   
     const deliveryOptionsId = cartItem.deliveryOptionsId;
   
-    let deliveryOption;
-  
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionsId){
-        deliveryOption = option;
-      }
-    });
+    let deliveryOption = getDeliveryOption(deliveryOptionsId);
   
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays,'days');
